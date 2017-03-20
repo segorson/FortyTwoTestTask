2a21bef6db4057a84e985ac11a055ec29e4fa865
segorson <segorson@gmail.com>
2017-03-20 20:13:33 +0200

    ticket:1 templates updated

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from apps.hello import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,6 +8,6 @@ urlpatterns = patterns(
     # Examples:
     # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
