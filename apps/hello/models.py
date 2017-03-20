19bdfce0746efce7cdaa4d1306af700f5f3553d3
segorson <segorson@gmail.com>
2017-03-20 20:11:26 +0200

    ticket:1 added models

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Bio(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    date_of_birth = models.DateField(blank=True, null=True)
+    bio = models.TextField(max_length=300, blank=True, null=True)
+    email = models.EmailField(max_length=30, blank=True, null=True)
+    jabber = models.CharField(max_length=30, blank=True, null=True)
+    skype = models.CharField(max_length=30, blank=True, null=True)
+    other_contacts = models.TextField(max_length=300, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
